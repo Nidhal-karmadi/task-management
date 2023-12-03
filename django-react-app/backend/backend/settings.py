e1a91d6eaff19be0ac0b10b24bdf0e6908c69df3
Nidhal-karmadi <125440528+Nidhal-karmadi@users.noreply.github.com>
2023-12-03 10:45:08 +0100

    alhamdoulillah
    alhamdoulillah

--- django-react-app/backend/backend/settings.py
+++ django-react-app/backend/backend/settings.py
@@ -123,5 +123,8 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- django-react-app/frontend/src/App.js
+++ django-react-app/frontend/src/App.js
@@ -1,65 +1,43 @@
-import React, { Component } from 'react'
-import './App.css';
-import CustomModal from './components/Modal';
-import axios from 'axios';
-
-
+import React, { Component } from "react";
+import Modal from "./components/Modal";
+import axios from 'axios';  
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state= {
-      modal: false,
-      viewCompleted:false,
+    this.state = {
+      viewCompleted: false,
       activeItem: {
         title: "",
         description: "",
         completed: false
       },
-      todoList: []
+      taskList: []
     };
   }
+
+  // Add componentDidMount()
   componentDidMount() {
-    this.refreshlist();
+    this.refreshList();
   }
-  refreshlist = () => {
-    axios
-    .get("http://localhost:8000/api/tasks")
-    .then(res => this.state({ todoList: res.data }))
-    .catch(err => console.log(err))
+
+ 
+  refreshList = () => {
+    axios   //Axios to send and receive HTTP requests
+      .get("http://localhost:8000/api/tasks/")
+      .then(res => this.setState({ taskList: res.data }))
+      .catch(err => console.log(err));
   };
-  toggle = () => {
-    this.setState({modal: !this.state.modal});
-  };
-  handleSubmit = item => {
-    this.toggle();
-    if (item.id) {
-      axios
-      .put('http://localhost:8000/api/tasks/${item.ad}/', item)
-      .then(res => this.refreshList())
-    }
-    axios
-    .post("http://localhost:8000/api/tasks/", item)
-    .then( res => this.refreshList())
-  };
-  handleDelete = item => {
-    axios
-      .delete('http://localhost:8000/api/tasks/${item.ad}/')
-      .then(res => this.refreshList())
-  };
-  createItem = () => {
-    const item = {title: "", modal: !this.state.modal};
-    this.setState({ activeItem: item, modal: !this.state.modal});
-  };
-  editItem = item => {
-    this.setState({activeItem: item, modal: !this.state.modal})
-  }
+
+
   displayCompleted = status => {
     if (status) {
       return this.setState({ viewCompleted: true });
     }
     return this.setState({ viewCompleted: false });
-  }
+  };
+
+
   renderTabList = () => {
     return (
       <div className="my-5 tab-list">
@@ -76,13 +54,13 @@ class App extends Component {
           Incompleted
             </span>
       </div>
-    )
-    
-  }
+    );
+  };
 
+  // Main variable to render items on the screen
   renderItems = () => {
     const { viewCompleted } = this.state;
-    const newItems = this.state.todoList.filter(
+    const newItems = this.state.taskList.filter(
       item => item.completed === viewCompleted
     );
     return newItems.map(item => (
@@ -93,30 +71,88 @@ class App extends Component {
         <span
           className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""
             }`}
-          title={item.title}
+          title={item.description}
         >
           {item.title}
         </span>
         <span>
-          <button className="btn btn-info mr-2">Edit</button>
-          <button className="btn btn-danger mr-2">Delete</button>
+          <button
+            onClick={() => this.editItem(item)}
+            className="btn btn-secondary mr-2"
+          >
+            Edit
+          </button>
+          <button
+            onClick={() => this.handleDelete(item)}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
         </span>
       </li>
-    ))
-  
-  
+    ));
+  };
+  // ///////////////////////////////////////////////////////////
+
+  ////add this after modal creation
+  toggle = () => {//add this after modal creation
+    this.setState({ modal: !this.state.modal });//add this after modal creation
+  };
+  // handleSubmit = item => {//add this after modal creation
+  //   this.toggle();//add this after modal creation
+  //   alert("save" + JSON.stringify(item));//add this after modal creation
+  // };
+
+  // Submit an item
+  handleSubmit = item => {
+    this.toggle();
+    if (item.id) {
+      // if old post to edit and submit
+      axios
+        .put(`http://localhost:8000/api/tasks/${item.id}/`, item)
+        .then(res => this.refreshList());
+      return;
+    }
+    // if new post to submit
+    axios
+      .post("http://localhost:8000/api/tasks/", item)
+      .then(res => this.refreshList());
   };
 
+  // Delete item
+  handleDelete = item => {
+    axios
+      .delete(`http://localhost:8000/api/tasks/${item.id}/`)
+      .then(res => this.refreshList());
+  };
+  // handleDelete = item => {//add this after modal creation
+  //   alert("delete" + JSON.stringify(item));//add this after modal creation
+  // };
+
+  // Create item
+  createItem = () => {
+    const item = { title: "", description: "", completed: false };
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+
+  //Edit item
+  editItem = item => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+
+
+  // -I- Start by visual effects to viewer
   render() {
     return (
-      <main className="content p-3 mb-2 bg-info">
-        <h1 className="text-white text-uppercase text-center my-4">Task Manager</h1>
+      <main className="content">
+        <h1 className="text-black text-uppercase text-center my-4">Task Manager</h1>
         <div className="row ">
           <div className="col-md-6 col-sm-10 mx-auto p-0">
             <div className="card p-3">
-              <div>
-                <button className="btn btn-warning">Add Task</button>
-
+              <div className="">
+                <button onClick={this.createItem} className="btn btn-primary">
+                  Add task
+                    </button>
               </div>
               {this.renderTabList()}
               <ul className="list-group list-group-flush">
@@ -125,15 +161,15 @@ class App extends Component {
             </div>
           </div>
         </div>
-        <footer className="my-3 mb-2 bg-info text-white text-center">Copyright 2023 &copy; karmadi nidhal and gift moyo</footer>
         {this.state.modal ? (
-          <modal activeItem={this.state.activeItem} toggle={this.toggle}
-          onSave={this.handleSubmit}/>
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
         ) : null}
-        
-      
       </main>
     );
-  };
+  }
 }
 export default App;
